0320b10be6546834a55bb6fe43e0134e2463a8af
Lucas Patrick Borges de Souza <lpatrick@ciandt.com>
2021-05-02 19:37:54 -0300

    adicionando arquivos iniciais

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Estudos-Angular';
+  logoGoogle    = '../../../../../assets/images/google-logo.png'
+  altLogoGoogle = 'Logo Google'
+  logoCiandt    = '../../../../../assets/images/ciandt-logo.svg'
+  altLogoCiandt = 'Logo ciet'
+}
